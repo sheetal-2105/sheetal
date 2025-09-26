12366d0ea4f067753e7dfbdb95f1f2d9a1d34a62
sheetal <sheetu1321@gmail.com>
2025-09-26 12:32:44 +0530

    first commit

---
+++ schedule.js
@@ -0,0 +1,7 @@
+console.log("Monday- busy till 2 p.m");
+console.log("Tuesday- get up at 9 sharp");
+console.log("Wednesday- get up its a hectic day today");
+console.log("Thursday- you can rest a bit maybe...");
+console.log("Friday- do a lots of work as its weekend tommorow");
+console.log("Saturday- its holiday");
+console.log("Sunday- its holiday");
